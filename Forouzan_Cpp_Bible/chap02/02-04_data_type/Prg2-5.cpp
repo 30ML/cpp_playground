ffd2a1d1b3ddcef7e777bea6159eb4856f7b1dfe
Minjae Jay Kim <exploit017@gmail.com>
2022-03-30 14:49:28 +0900

    계좌 잔액 프로그램

---
+++ Forouzan_Cpp_Bible/chap02/02-04_data_type/Prg2-5.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // 변수 선언
+  int balance = 0;
+  int transaction;
+
+  // 첫 번째 거래 후 잔액 조정
+  cout << "첫 번째 거래 금액 입력: ";
+  cin >> transaction;
+  balance = balance + transaction;
+
+  // 두 번째 거래 후 잔액 조정
+  cout << "두 번째 거래 금액 입력: ";
+  cin >> transaction;
+  balance = balance + transaction;
+
+  // 세 번째 거래 후 잔액 조정
+  cout << "세 번째 거래 금액 입력: ";
+  cin >> transaction;
+  balance = balance + transaction;
+
+  // 최종 잔액 출력
+  cout << "계좌의 최종 잔액은 " << balance << "달러입니다.";
+
+  return 0;
+}
