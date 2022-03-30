1349b8c7ff5ed51cd01c999b46403287a6c010a0
Minjae Jay Kim <exploit017@gmail.com>
2022-03-30 16:00:43 +0900

    이스케이프 문자 확인하기

---
+++ Forouzan_Cpp_Bible/chap02/02-04_data_type/Prg2-10.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Hello\n";
+  cout << "Hi\t friends." << endl;
+  cout << "Buenos dias  \bamigos." << endl; // 중간에 2칸 띄어쓰기 함.
+  cout << "Hello\rBonjour mes amis." << endl;
+  cout << "This is a single quote \'." << endl;
+  cout << "This is a double quote \"." << endl;
+  cout << "This is how to print a backslash \\.";
+
+  return 0;
+}
